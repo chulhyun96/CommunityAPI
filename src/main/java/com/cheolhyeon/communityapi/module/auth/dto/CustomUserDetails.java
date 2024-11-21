c02342e8ccc509287e06ad2166106f76ac327f9c
박철현 <park82602@gmail.com>
2024-11-22 01:52:58 +0900

    feat : UserDetails를 구현한 클래스 추가

---
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/dto/CustomUserDetails.java
@@ -0,0 +1,34 @@
+package com.cheolhyeon.communityapi.module.auth.dto;
+
+import com.cheolhyeon.communityapi.module.auth.entity.Users;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final Users users;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> role = new ArrayList<>();
+        role.add(new SimpleGrantedAuthority(users.getRole()));
+        return role;
+    }
+
+    @Override
+    public String getPassword() {
+        return users.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return users.getUsername();
+    }
+}
