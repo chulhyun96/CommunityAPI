e0972369a8c8bff49156758143e75198cccb74c0
박철현 <park82602@gmail.com>
2024-11-22 19:09:58 +0900

    feat : Users Entity 구현

--- src/main/java/com/cheolhyeon/communityapi/module/auth/entity/Users.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/entity/Users.java
@@ -1,29 +1,33 @@
 package com.cheolhyeon.communityapi.module.auth.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.cheolhyeon.communityapi.module.auth.type.AuthorityPolicy;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-public class Users {
+public class Users extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 10, unique = true)
     private String username;
+
+    @Column(nullable = false, length = 10)
     private String password;
+
+    @Column(nullable = false, length = 15)
     private String phoneNumber;
-    private String role;
-    private LocalDate createdAt;
-    private LocalDate modifiedAt;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AuthorityPolicy role;
 }
