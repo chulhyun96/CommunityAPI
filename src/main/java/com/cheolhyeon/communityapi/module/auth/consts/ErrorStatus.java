98dd6a322627cf1197e883e1b100e8b41d4a2865
박철현 <park82602@gmail.com>
2024-11-22 21:30:47 +0900

    feat : ErrorStatus 추가

--- src/main/java/com/cheolhyeon/communityapi/module/auth/consts/ErrorStatus.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/consts/ErrorStatus.java
@@ -5,7 +5,10 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorStatus {
-    ACCOUNT_NOT_EXISTS(HttpStatus.BAD_REQUEST,"계정이 존재 하지 않습니다.");
+    ACCOUNT_NOT_EXISTS(HttpStatus.BAD_REQUEST,"계정이 존재 하지 않습니다."),
+    ACCOUNT_HAS_NOT_ROLE(HttpStatus.INTERNAL_SERVER_ERROR, "알 수 없는 오류 발생."),
+    JSON_PARSING_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 데이터 파싱중 오류 발생");
+
 
     private final HttpStatus status;
     private final String message;
@@ -14,5 +17,4 @@ public enum ErrorStatus {
         this.status = status;
         this.message = message;
     }
-
 }
