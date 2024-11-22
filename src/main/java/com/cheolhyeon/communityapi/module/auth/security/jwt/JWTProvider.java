ccb24a2d8e861e3b69a18564ce98eb3c8c8ae32d
박철현 <park82602@gmail.com>
2024-11-22 21:34:25 +0900

    chore : TODO 파일 작성

--- src/main/java/com/cheolhyeon/communityapi/module/auth/security/jwt/JWTProvider.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/security/jwt/JWTProvider.java
@@ -3,7 +3,6 @@ package com.cheolhyeon.communityapi.module.auth.security.jwt;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
