a4eed35bc1729bf59eea615bebf2b77e71364839
박철현 <park82602@gmail.com>
2024-11-22 19:54:54 +0900

    feat : 요청 시 JWT를 검증하는 로직 구현

--- src/main/java/com/cheolhyeon/communityapi/module/auth/security/jwt/JWTPerRequestFilter.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/security/jwt/JWTPerRequestFilter.java
@@ -1,11 +1,18 @@
 package com.cheolhyeon.communityapi.module.auth.security.jwt;
 
+import com.cheolhyeon.communityapi.module.auth.dto.CustomUserDetails;
+import com.cheolhyeon.communityapi.module.auth.entity.Users;
+import com.cheolhyeon.communityapi.module.auth.type.AuthorityPolicy;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -21,5 +28,46 @@ public class JWTPerRequestFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (isInvalidToken(bearerToken)) {
+            log.info("Bearer token is Invalid");
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = getToken(bearerToken);
+        if(jwtProvider.isExpired(token)) {
+            log.info("Bearer token is Expired");
+            handleExpiredToken(request, response);
+            return;
+        }
+        Authentication auth = createAuthentication(token);
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        filterChain.doFilter(request, response);
+    }
+
+    private Authentication createAuthentication(String token) {
+        String username = jwtProvider.getUsername(token);
+        AuthorityPolicy role = AuthorityPolicy.fromString(jwtProvider.getRoleAsString(token));
+        Users userEntity = Users.builder()
+                .username(username)
+                .role(role)
+                .build();
+        CustomUserDetails customUser = CustomUserDetails.from(userEntity);
+        return new UsernamePasswordAuthenticationToken(customUser, null, customUser.getAuthorities());
+    }
+
+    private static String getToken(String bearerToken) {
+        return bearerToken.substring(7);
+    }
+
+    private static void handleExpiredToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().invalidate();
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setHeader("Location", "/login");
+        response.getWriter().write("Token expired. Please log in.");
+    }
+
+    private static boolean isInvalidToken(String bearerToken) {
+        return !StringUtils.hasText(bearerToken) || !StringUtils.startsWithIgnoreCase(bearerToken, "Bearer");
     }
 }
