d0500871f556735bf5fb1fc8befcdfce668aa442
박철현 <park82602@gmail.com>
2024-11-25 23:51:03 +0900

    feat : 팩토리 생성자 메서드 생성

--- src/main/java/com/cheolhyeon/communityapi/module/auth/entity/Users.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/entity/Users.java
@@ -1,5 +1,6 @@
 package com.cheolhyeon.communityapi.module.auth.entity;
 
+import com.cheolhyeon.communityapi.module.auth.dto.AuthRequest;
 import com.cheolhyeon.communityapi.module.auth.type.AuthorityPolicy;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -21,7 +22,7 @@ public class Users extends BaseEntity{
     @Column(nullable = false, length = 10, unique = true)
     private String username;
 
-    @Column(nullable = false, length = 10)
+    @Column(nullable = false)
     private String password;
 
     @Column(nullable = false, length = 15)
@@ -30,6 +31,15 @@ public class Users extends BaseEntity{
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private AuthorityPolicy role;
+
+    public static Users create(AuthRequest request, String encodedPassword) {
+        return Users.builder()
+                .username(request.getUsername())
+                .password(encodedPassword)
+                .phoneNumber(request.getPhoneNumber())
+                .role(AuthorityPolicy.ROLE_USER)
+                .build();
+    }
 
 
     public String getRoleAsString() {
