0560bc537cd0e9a2294ead383fa08c1f028d5179
박철현 <park82602@gmail.com>
2024-11-25 23:52:24 +0900

    feat : 회원가입 기능 구현

--- src/main/java/com/cheolhyeon/communityapi/module/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/service/CustomUserDetailsService.java
@@ -1,23 +1,42 @@
 package com.cheolhyeon.communityapi.module.auth.service;
 
+import com.cheolhyeon.communityapi.module.auth.dto.AuthRequest;
 import com.cheolhyeon.communityapi.module.auth.dto.CustomUserDetails;
 import com.cheolhyeon.communityapi.module.auth.entity.Users;
+import com.cheolhyeon.communityapi.module.auth.exception.UserAlreadyExistException;
 import com.cheolhyeon.communityapi.module.auth.repository.UsersRepository;
+import com.cheolhyeon.communityapi.module.auth.type.ErrorStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Users findUser = usersRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
 
         return CustomUserDetails.from(findUser);
+    }
+
+    @Transactional
+    public Users save(AuthRequest request) {
+        usersRepository.findByUsername(request.getUsername())
+                .ifPresent(it -> {
+                    throw new UserAlreadyExistException(ErrorStatus.USER_ALREADY_EXIST);
+                });
+
+        return usersRepository.save(Users.create(request,
+                passwordEncoder.encode(request.getPassword())));
     }
 }
