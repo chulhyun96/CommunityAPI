6bfdea1e562c7a5210f423b23cd3d5cbf93f5a2b
박철현 <park82602@gmail.com>
2024-11-25 15:04:32 +0900

    feat : PasswordEncoder 추가

--- src/main/java/com/cheolhyeon/communityapi/module/auth/security/SecurityConfig.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/security/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -38,6 +40,11 @@ public class SecurityConfig {
     private final ObjectMapper objectMapper;
     private final JWTProvider jwtProvider;
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
