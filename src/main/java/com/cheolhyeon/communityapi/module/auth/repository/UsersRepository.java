75c879ed002c60a8e7f9fbbd7796fe45467832f2
박철현 <park82602@gmail.com>
2024-11-25 23:50:14 +0900

    feat : UsersRepository DAO 생성

---
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.cheolhyeon.communityapi.module.auth.repository;
+
+import com.cheolhyeon.communityapi.module.auth.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findByUsername(String username);
+}
