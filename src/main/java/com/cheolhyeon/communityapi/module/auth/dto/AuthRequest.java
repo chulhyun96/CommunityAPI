4b271edc0e18e3eaa5846ba429fece73a09a2b38
박철현 <park82602@gmail.com>
2024-11-26 01:53:28 +0900

    refactor : NotNull -> NotBlank로 수정

--- src/main/java/com/cheolhyeon/communityapi/module/auth/dto/AuthRequest.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/dto/AuthRequest.java
@@ -1,23 +1,21 @@
 package com.cheolhyeon.communityapi.module.auth.dto;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class AuthRequest {
-    @NotNull
+    @NotBlank(message = "아이디를 입력해주세요.")
     @Size(min = 1, max = 10, message = "회원의 아이디는 1~10글자 사이여야합니다.")
     private String username;
 
-    @NotNull
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     @Size(min = 7, max = 15, message = "비밀번호는 최소 7~15글자 사이여야합니다.")
     private String password;
 
-    @NotNull
-    @Pattern(regexp = "^01[016789]-\\d{3,4}-\\d{4}$", message = "핸드폰 번호 형식에 어긋납니다.")
+    @NotBlank(message = "핸드폰 번호를 입력해주세요.")
+    @Pattern(regexp = "^01[016789]-\\d{3,4}-\\d{4}$", message = "입력 형식에 어긋납니다.")
     private String phoneNumber;
 }
