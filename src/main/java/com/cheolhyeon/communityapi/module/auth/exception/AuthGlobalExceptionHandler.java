a0f0a82bb13b1089ed9ed599adfbe4e2f663a0fa
박철현 <park82602@gmail.com>
2024-11-26 01:52:56 +0900

    feat : AuthGlobalExceptionHandler.java AOP가 AuthController에서만 적용될 수 있도록 범위를 지정함

--- src/main/java/com/cheolhyeon/communityapi/module/auth/exception/AuthGlobalExceptionHandler.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/exception/AuthGlobalExceptionHandler.java
@@ -1,10 +1,11 @@
 package com.cheolhyeon.communityapi.module.auth.exception;
 
+import com.cheolhyeon.communityapi.module.auth.controller.AuthController;
 import com.cheolhyeon.communityapi.module.auth.dto.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(assignableTypes = AuthController.class)
 public class AuthGlobalExceptionHandler {
 
     @ExceptionHandler(UserAlreadyExistException.class)
