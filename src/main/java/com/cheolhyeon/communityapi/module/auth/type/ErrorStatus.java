7ddd7106e516576399c2cdee1f45dac57a3f7a90
박철현 <park82602@gmail.com>
2024-11-25 22:57:12 +0900

    feat : 사용자 중복시에 대한 ErrorType 추가

--- src/main/java/com/cheolhyeon/communityapi/module/auth/type/ErrorStatus.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/type/ErrorStatus.java
@@ -19,16 +19,20 @@ public enum ErrorStatus {
             HttpStatus.INTERNAL_SERVER_ERROR,
             HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
             "JSON Paring 오류 발생"
+    ),
+    USER_ALREADY_EXIST(HttpStatus.CONFLICT,
+            HttpStatus.CONFLICT.getReasonPhrase(),
+            "사용자 조회 - ID 중복입니다."
     );
 
 
-    private final HttpStatus status;
-    private final String error;
+    private final int errorCode;
+    private final String errorType;
     private final String message;
 
-    ErrorStatus(HttpStatus status, String error, String message) {
-        this.status = status;
-        this.error = error;
+    ErrorStatus(HttpStatus status, String errorType, String message) {
+        this.errorCode = status.value();
+        this.errorType = errorType;
         this.message = message;
     }
 }
