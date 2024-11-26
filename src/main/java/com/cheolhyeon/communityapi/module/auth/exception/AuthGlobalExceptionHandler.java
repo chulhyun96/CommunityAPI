f19f95303ad271f7366f2e3c51b871177fa1dabc
박철현 <park82602@gmail.com>
2024-11-26 20:24:37 +0900

    feat : RestControllerAdvice 적용 범위 추가

--- src/main/java/com/cheolhyeon/communityapi/module/auth/exception/AuthGlobalExceptionHandler.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/exception/AuthGlobalExceptionHandler.java
@@ -1,15 +1,16 @@
 package com.cheolhyeon.communityapi.module.auth.exception;
 
 import com.cheolhyeon.communityapi.module.auth.controller.AuthController;
-import com.cheolhyeon.communityapi.module.auth.dto.ErrorResponse;
+import com.cheolhyeon.communityapi.module.auth.controller.UserController;
+import com.cheolhyeon.communityapi.module.auth.dto.error.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(assignableTypes = AuthController.class)
+@RestControllerAdvice(assignableTypes = {AuthController.class, UserController.class})
 public class AuthGlobalExceptionHandler {
 
-    @ExceptionHandler(UserAlreadyExistException.class)
-    public ErrorResponse userAlreadyExistHandler(UserAlreadyExistException e) {
+    @ExceptionHandler(UserException.class)
+    public ErrorResponse userExceptionHandler(UserException e) {
         return ErrorResponse.of(e.getErrorStatus());
     }
 }
