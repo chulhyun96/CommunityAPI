39a4fd7e8a0dfe1544e577b79c4ea3232dc48745
박철현 <park82602@gmail.com>
2024-11-26 20:27:18 +0900

    feat : User 조회 엔드포인트 추가

---
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.cheolhyeon.communityapi.module.auth.controller;
+
+import com.cheolhyeon.communityapi.module.auth.dto.user.UserResponse;
+import com.cheolhyeon.communityapi.module.auth.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<?> getUserDetail(@PathVariable Long id) {
+        return ResponseEntity.ok().body(UserResponse.from(
+                customUserDetailsService.getUser(id)
+        ));
+    }
+}
