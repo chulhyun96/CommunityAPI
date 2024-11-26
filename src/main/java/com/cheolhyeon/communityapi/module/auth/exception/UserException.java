c615080757674c3a93eb0b9d61b5d2b40b9c71e6
박철현 <park82602@gmail.com>
2024-11-26 20:25:20 +0900

    style : UserAlreadyExistException -> UserException 으로 클래스명 변경

--- src/main/java/com/cheolhyeon/communityapi/module/auth/exception/UserException.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/exception/UserException.java
@@ -4,12 +4,12 @@ import com.cheolhyeon.communityapi.module.auth.type.ErrorStatus;
 import lombok.Getter;
 
 @Getter
-public class UserAlreadyExistException extends RuntimeException {
+public class UserException extends RuntimeException {
     private final ErrorStatus errorStatus;
     private final String error;
     private final int errorCode;
 
-    public UserAlreadyExistException(ErrorStatus errorStats) {
+    public UserException(ErrorStatus errorStats) {
         super(errorStats.getMessage());
         this.errorStatus = errorStats;
         this.errorCode = errorStats.getErrorCode();
