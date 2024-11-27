ed472b872deb59b4a4ef5191a3efe773826e72ff
박철현 <park82602@gmail.com>
2024-11-28 00:32:03 +0900

    feat : PostResponse DTO 클래스 필드 추가

--- src/main/java/com/cheolhyeon/communityapi/module/post/dto/PostResponse.java
+++ src/main/java/com/cheolhyeon/communityapi/module/post/dto/PostResponse.java
@@ -3,6 +3,7 @@ package com.cheolhyeon.communityapi.module.post.dto;
 import com.cheolhyeon.communityapi.module.post.entity.Post;
 import lombok.Getter;
 
+import java.time.LocalDate;
 
 @Getter
 public class PostResponse {
@@ -10,12 +11,14 @@ public class PostResponse {
     private final String title;
     private final String content;
     private final Integer commentCount;
+    private final LocalDate createdAt;
 
     private PostResponse(Post post) {
         this.username = post.getUser().getUsername();
         this.title = post.getTitle();
         this.content = post.getContent();
         this.commentCount = post.getCommentCount();
+        this.createdAt = post.getCreatedAt();
     }
 
     public static PostResponse create(Post post) {
