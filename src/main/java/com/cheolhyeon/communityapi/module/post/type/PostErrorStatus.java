78b9bb9446e6ead2805ffbe064c2fd033b652144
박철현 <park82602@gmail.com>
2024-11-28 00:27:15 +0900

    feat : 게시글에 대한 Exception의 상태를 정의할 enum 클래스 생성

---
+++ src/main/java/com/cheolhyeon/communityapi/module/post/type/PostErrorStatus.java
@@ -0,0 +1,26 @@
+package com.cheolhyeon.communityapi.module.post.type;
+
+import com.cheolhyeon.communityapi.module.common.ErrorStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum PostErrorStatus implements ErrorStatus {
+    POST_NOT_FOUND(
+            HttpStatus.NOT_FOUND,
+            HttpStatus.NOT_FOUND.getReasonPhrase(),
+            "해당 게시글은 존재 하지 않습니다."
+    );
+
+    private final int errorCode;
+    private final String errorType;
+    private final String message;
+
+    PostErrorStatus(HttpStatus status, String errorType, String message) {
+        this.errorCode = status.value();
+        this.errorType = errorType;
+        this.message = message;
+    }
+}
