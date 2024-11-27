79df9853e2c2ebcefe0b49d19bbf2bdd2f7e8350
박철현 <park82602@gmail.com>
2024-11-27 21:47:18 +0900

    test : 테스트 코드 수정

--- src/test/java/com/cheolhyeon/communityapi/module/auth/service/CustomUserDetailsServiceTest.java
+++ src/test/java/com/cheolhyeon/communityapi/module/auth/service/CustomUserDetailsServiceTest.java
@@ -133,6 +133,7 @@ class CustomUserDetailsServiceTest {
     void fail_get_user() {
         //given
         given(usersRepository.findById(anyLong())).willReturn(Optional.empty());
+        given(usersRepository.findByUsername(anyString())).willReturn(Optional.empty());
         //when
         assertThrows(UserException.class, () -> customUserDetailsService.getUser(1L,"Test1"));
         //then
