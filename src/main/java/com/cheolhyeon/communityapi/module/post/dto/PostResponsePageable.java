06fb2c42b9f3f4b32ba9c3e968e63fc335e53e27
박철현 <park82602@gmail.com>
2024-11-28 01:53:27 +0900

    refactor : 전체 게시글 목록 조회 시 댓글 수와, 게시글 작성 날짜도 보일 수 있도록 추가

--- src/main/java/com/cheolhyeon/communityapi/module/post/dto/PostResponsePageable.java
+++ src/main/java/com/cheolhyeon/communityapi/module/post/dto/PostResponsePageable.java
@@ -3,17 +3,23 @@ package com.cheolhyeon.communityapi.module.post.dto;
 import com.cheolhyeon.communityapi.module.post.entity.Post;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 
 @Getter
 public class PostResponsePageable {
     private final Long postId;
     private final String username;
     private final String title;
+    private final Integer commentCount;
+    private final LocalDate createdDate;
 
     private PostResponsePageable(Post post) {
         this.postId = post.getId();
         this.username = post.getUser().getUsername();
         this.title = post.getTitle();
+        this.createdDate = post.getCreatedAt();
+        this.commentCount = post.getCommentCount();
     }
 
     public static PostResponsePageable create(Post post) {
