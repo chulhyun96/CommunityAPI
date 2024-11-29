0f3b1668b522c0d7607a888f80d7de92a4280881
박철현 <park82602@gmail.com>
2024-11-30 03:01:03 +0900

    refactor : JWT를 가지고 자신의 회원 정보만 볼 수 있도록 수정

--- src/main/java/com/cheolhyeon/communityapi/module/auth/controller/UserController.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/controller/UserController.java
@@ -1,23 +1,21 @@
 package com.cheolhyeon.communityapi.module.auth.controller;
 
+import com.cheolhyeon.communityapi.module.auth.dto.CustomUserDetails;
 import com.cheolhyeon.communityapi.module.auth.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final CustomUserDetailsService customUserDetailsService;
 
-    @GetMapping("/user/{id}")
-    public ResponseEntity<?> getUserDetail(@PathVariable Long id,
-                                           Principal principal) {
-        return ResponseEntity.ok(customUserDetailsService.getUser(
-                id, principal.getName()));
+    @GetMapping("/user")
+    public ResponseEntity<?> getUserDetail(Authentication authentication) {
+        CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
+        return ResponseEntity.ok(customUserDetailsService.getUser(user.getUsername()));
     }
 }
