115cc2f782df0137dea054260ae874267a365144
박철현 <park82602@gmail.com>
2024-11-29 20:37:45 +0900

    feat : Comment 엔티티 생성

---
+++ src/main/java/com/cheolhyeon/communityapi/module/comment/entity/Comment.java
@@ -0,0 +1,36 @@
+package com.cheolhyeon.communityapi.module.comment.entity;
+
+import com.cheolhyeon.communityapi.module.auth.entity.Users;
+import com.cheolhyeon.communityapi.module.post.entity.Post;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
