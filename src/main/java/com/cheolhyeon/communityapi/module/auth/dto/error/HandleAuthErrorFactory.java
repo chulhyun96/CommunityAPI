de491f48df0049bbc676b2a860a9c0284d676d17
박철현 <park82602@gmail.com>
2024-11-29 19:33:36 +0900

    feat : HandleAuthErrorFactory 클래스 생성
    - 토큰 만료 및 로그인 하지 않은 사용자가 로그인 해야 이용할 수 있는 서비스 접근 시 에러 생성 - JSON으로 반환

---
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/dto/error/HandleAuthErrorFactory.java
@@ -0,0 +1,23 @@
+package com.cheolhyeon.communityapi.module.auth.dto.error;
+
+import com.cheolhyeon.communityapi.module.auth.type.AuthErrorStatus;
+import com.cheolhyeon.communityapi.module.auth.util.JsonPrettyPrinter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public final class HandleAuthErrorFactory {
+
+    private HandleAuthErrorFactory() {
+        throw new AssertionError("Util Class");
+    }
+
+    public static String getErrorResponse(ObjectMapper objectMapper, AuthErrorStatus errorStatus) throws JsonProcessingException {
+        if (errorStatus == AuthErrorStatus.LOGIN_FIRST) {
+            return JsonPrettyPrinter.getPrettyForPrint(objectMapper,
+                    ErrorResponse.create(AuthErrorStatus.LOGIN_FIRST));
+        }
+        return JsonPrettyPrinter.getPrettyForPrint(objectMapper,
+                ErrorResponse.create(AuthErrorStatus.TOKEN_EXPIRED));
+    }
+}
