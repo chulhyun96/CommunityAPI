a701cda782ea7c48c150b25fc0bc6cf0cde988a9
박철현 <park82602@gmail.com>
2024-11-29 18:42:10 +0900

    refactor : isLast 기능 삭제

--- src/main/java/com/cheolhyeon/communityapi/module/post/dto/PaginatedResponse.java
+++ src/main/java/com/cheolhyeon/communityapi/module/post/dto/PaginatedResponse.java
@@ -12,7 +12,6 @@ public class PaginatedResponse<T> {
     private final int size;
     private final long totalElements;
     private final int totalPages;
-    private final boolean last;
 
     private PaginatedResponse(Page<T> page) {
         this.content = page.getContent();
@@ -20,7 +19,6 @@ public class PaginatedResponse<T> {
         this.size = page.getSize();
         this.totalElements = page.getTotalElements();
         this.totalPages = page.getTotalPages();
-        this.last = page.isLast();
     }
     public static <T> PaginatedResponse<T> create(Page<T> page) {
         return new PaginatedResponse<>(page);
