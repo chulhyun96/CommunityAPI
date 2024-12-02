34a6d52744c894e504f3df286580e0b3b5d8a8d4
박철현 <park82602@gmail.com>
2024-12-02 15:50:04 +0900

    style : @EnableSpringDataWebSupport 기능 애노테이션 제거

--- src/main/java/com/cheolhyeon/communityapi/CommunityApiApplication.java
+++ src/main/java/com/cheolhyeon/communityapi/CommunityApiApplication.java
@@ -3,12 +3,9 @@ package com.cheolhyeon.communityapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @EnableJpaAuditing
 public class CommunityApiApplication {
 
