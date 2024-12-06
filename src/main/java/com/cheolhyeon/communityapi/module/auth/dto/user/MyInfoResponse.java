92499e97c234984487460e187e9bc6b9be79bdf7
박철현 <park82602@gmail.com>
2024-12-07 02:43:49 +0900

    style : 클래스명 변경

--- src/main/java/com/cheolhyeon/communityapi/module/auth/dto/user/MyInfoResponse.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/dto/user/MyInfoResponse.java
@@ -2,20 +2,17 @@ package com.cheolhyeon.communityapi.module.auth.dto.user;
 
 import com.cheolhyeon.communityapi.module.auth.entity.Users;
 import com.cheolhyeon.communityapi.module.auth.type.AuthorityPolicy;
-import com.cheolhyeon.communityapi.module.post.entity.Post;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
-public class UserResponse {
+public class MyInfoResponse {
     private final String username;
     private final String phoneNumber;
     private final AuthorityPolicy role;
     private final Integer countOfPosts;
     private final Integer countOfComments;
 
-    private UserResponse(Users user) {
+    private MyInfoResponse(Users user) {
         this.username = user.getUsername();
         this.phoneNumber = maskPhoneNumber(user.getPhoneNumber());
         this.role = user.getRole();
@@ -23,8 +20,8 @@ public class UserResponse {
         this.countOfComments = user.getComments().size();
     }
 
-    public static UserResponse create(Users user) {
-        return new UserResponse(user);
+    public static MyInfoResponse create(Users user) {
+        return new MyInfoResponse(user);
     }
 
     private String maskPhoneNumber(String phoneNumber) {
