e84f02ceec1ce6ea400bdd720ba5ce700fe4ec18
박철현 <park82602@gmail.com>
2024-12-07 03:05:03 +0900

    style : 클래스명 변경

--- src/main/java/com/cheolhyeon/communityapi/module/auth/dto/user/GeneralUserInfoResponse.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/dto/user/GeneralUserInfoResponse.java
@@ -4,17 +4,17 @@ import com.cheolhyeon.communityapi.module.auth.entity.Users;
 import lombok.Getter;
 
 @Getter
-public class GeneralUserInfo {
+public class GeneralUserInfoResponse {
     private final String username;
     private final int countOfPosts;
     private final int countOfComments;
 
-    private GeneralUserInfo(Users user) {
+    private GeneralUserInfoResponse(Users user) {
         this.username = user.getUsername();
         this.countOfPosts = user.getPosts().size();
         this.countOfComments = user.getComments().size();
     }
-    public static GeneralUserInfo create(Users user) {
-        return new GeneralUserInfo(user);
+    public static GeneralUserInfoResponse create(Users user) {
+        return new GeneralUserInfoResponse(user);
     }
 }
