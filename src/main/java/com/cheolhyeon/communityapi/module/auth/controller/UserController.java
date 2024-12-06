4ba5bfd8dd70844db35cbe399dac7fee7d3d2199
박철현 <park82602@gmail.com>
2024-12-07 02:13:46 +0900

    style : 주석 추가

--- src/main/java/com/cheolhyeon/communityapi/module/auth/controller/UserController.java
+++ src/main/java/com/cheolhyeon/communityapi/module/auth/controller/UserController.java
@@ -6,16 +6,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
-    private final CustomUserDetailsService customUserDetailsService;
+    private final CustomUserDetailsService userService;
 
+    /**
+     * 내 정보 보기
+     */
     @GetMapping("/user")
     public ResponseEntity<?> getUserDetail(Authentication authentication) {
         CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
-        return ResponseEntity.ok(customUserDetailsService.getUser(user.getUsername()));
+        return ResponseEntity.ok(userService.getMyInfo(user.getUsername()));
     }
 }
