10abf6cd29fe0516a1c8261b19f387ae4405a511
박철현 <park82602@gmail.com>
2024-12-12 19:48:53 +0900

    feat : setter 추가

--- src/main/java/com/cheolhyeon/communityapi/common/BaseEntity.java
+++ src/main/java/com/cheolhyeon/communityapi/common/BaseEntity.java
@@ -3,6 +3,7 @@ package com.cheolhyeon.communityapi.common;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDate;
 
 @Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
